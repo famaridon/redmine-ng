38c891c27dec47031528e14c827b61053779bf3c
famaridon <famaridon@visiativ.com>
2017-09-01 11:08:31 +0200

    add pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SidebarNavService} from './sidebar-nav.service'
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarNavComponent } from './sidebar-nav/sidebar-nav.component';
+import { SettingsComponent } from './settings/settings.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'settings', component: SettingsComponent },
+  { path: '',
+    redirectTo: '/settings',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarNavComponent
+    SidebarNavComponent,
+    SettingsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [
     SidebarNavService
