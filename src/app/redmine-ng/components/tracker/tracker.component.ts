bca06affcdb5d9510f8221e5f98db47390a86615
famaridon <famaridon@visiativ.com>
2017-12-21 14:03:20 +0100

    solve tracker select issue when use ngModel

--- src/app/redmine-ng/components/tracker/tracker.component.ts
+++ src/app/redmine-ng/components/tracker/tracker.component.ts
@@ -1,12 +1,18 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, forwardRef, Input, OnInit} from '@angular/core';
 import {Tracker} from '../../services/beans';
 import {RedmineService} from '../../services/redmine.service';
 import {SiSelectComponent} from '../../../states-inputs/components/si-select/si-select.component';
+import {NG_VALUE_ACCESSOR} from "@angular/forms";
 
 @Component({
   selector: 'rm-ng-tracker',
   templateUrl: './tracker.component.html',
-  styleUrls: ['./tracker.component.css']
+  styleUrls: ['./tracker.component.css'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => RmNgTrackerComponent),
+    multi: true
+  }]
 })
 export class RmNgTrackerComponent extends SiSelectComponent<Tracker> implements OnInit {
 
