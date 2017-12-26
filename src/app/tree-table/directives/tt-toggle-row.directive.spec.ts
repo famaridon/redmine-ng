27a8cda323243351f4e3dc0fadbbf0764b55abcb
famaridon <famaridon@visiativ.com>
2017-12-26 14:44:45 +0100

    toggle button

---
+++ src/app/tree-table/directives/tt-toggle-row.directive.spec.ts
@@ -0,0 +1,8 @@
+import { TtToggleRowDirective } from './tt-toggle-row.directive';
+
+describe('TtToggleRowDirective', () => {
+  it('should create an instance', () => {
+    const directive = new TtToggleRowDirective();
+    expect(directive).toBeTruthy();
+  });
+});
